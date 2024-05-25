1e7e759e1f5d6e89f8dc40650d5e741493b3e93d
ARISTAHS <paran0907@naver.com>
2024-05-25 21:14:17 +0900

    login input 입력 값 체크 로직 구현중

--- src/app/login/login_remeber.js
+++ src/app/login/login_remeber.js
@@ -39,28 +39,28 @@ export default function LoginRemember(){
   },[]); //한번만 실행
 
   function setCookie(cookieName, value, exdays) {
-    var exdate = new Date();
+    let exdate = new Date();
     exdate.setDate(exdate.getDate() + exdays);
     // var cookieValue = escape(value) + ((exdays == null) ? "" : "; expires=" + exdate.toGMTString());
     // escape 와 unescape 함수 사용 안함. 함수 변경
-    var cookieValue = encodeURIComponent(value) + ((exdays == null) ? "" : "; expires=" + exdate.toGMTString());
+    let cookieValue = encodeURIComponent(value) + ((exdays == null) ? "" : "; expires=" + exdate.toGMTString());
     document.cookie = cookieName + "=" + cookieValue;
   }
 
   function deleteCookie(cookieName) {
-    var expireDate = new Date();
+    let expireDate = new Date();
     expireDate.setDate(expireDate.getDate() - 1);
     document.cookie = cookieName + "= " + "; expires=" + expireDate.toGMTString();
   }
 
   function getCookie(cookieName) {
     cookieName = cookieName + '=';
-    var cookieData = document.cookie;
-    var start = cookieData.indexOf(cookieName);
-    var cookieValue = '';
+    let cookieData = document.cookie;
+    let start = cookieData.indexOf(cookieName);
+    let cookieValue = '';
     if (start != -1) {
       start += cookieName.length;
-      var end = cookieData.indexOf(';', start);
+      let end = cookieData.indexOf(';', start);
       if (end == -1) end = cookieData.length;
       // cookieValue = cookieData.substring(start, end);
       cookieValue = decodeURIComponent(cookieData.substring(start, end));
